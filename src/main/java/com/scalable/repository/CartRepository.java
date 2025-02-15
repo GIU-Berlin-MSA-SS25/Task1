6c2519c900951d32f27effc64f89b394ee9c2967
ZiadHegazy <ziadayman9901@gmail.com>
2025-02-15 12:16:35 +0200

    aa

--- src/main/java/com/scalable/repository/CartRepository.java
+++ src/main/java/com/scalable/repository/CartRepository.java
@@ -1,51 +1,25 @@
 package com.scalable.repository;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.io.File;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.scalable.model.Cart;
 
 @Component
 public class CartRepository {
-    @Value("${spring.application.data_path}")
-    private String FILE_PATH;
-    private ObjectMapper objectMapper = new ObjectMapper();
+
+    public static List< Cart> carts = new ArrayList<>();
+
     public CartRepository() {
-        
+        carts = new ArrayList<Cart>();
+        carts.add(new Cart(1,1,new ArrayList<String>(Arrays.asList("item1","item2"))));
+        carts.add(new Cart(2,2,new ArrayList<String>(Arrays.asList("item3","item4"))));
+        carts.add(new Cart(3,3,new ArrayList<String>(Arrays.asList("item5","item6"))));
     }
     public void addCart(Cart cart) {
-         try {
-            File file = new File(FILE_PATH);
-            ArrayList<Cart> carts;
-            if (!file.exists()) {
-                carts = new ArrayList<>();
-            } else {
-                carts = new ArrayList<>(Arrays.asList(objectMapper.readValue(file, Cart[].class)));
-            }
-            carts.add(cart);
-            objectMapper.writeValue(file, carts);
-            
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to write to JSON file", e);
-        }
+        carts.add(cart);
     }
-    public ArrayList<Cart> getCarts() {
-        try {
-            File file = new File(FILE_PATH);
-            if (!file.exists()) {
-                return new ArrayList<>();
-            }
-            return new ArrayList<Cart>(Arrays.asList(objectMapper.readValue(file, Cart[].class)));
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to read from JSON file", e);
-        }
-    }
-
     
 }
