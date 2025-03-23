ce8c7e41963173defc5ede325e39dd5e37185c89
Mohamed-Mohy-ElGamal <mohamed.mohy.elgamal@gmail.com>
2025-03-23 18:43:52 +0100

    Update AppApplicationTests.java
    Remove unused imports

--- src/test/java/com/scalable/app/AppApplicationTests.java
+++ src/test/java/com/scalable/app/AppApplicationTests.java
@@ -1,68 +1,61 @@
 // package com.scalable.app;
-
+//
 // import static org.junit.jupiter.api.Assertions.assertEquals;
 // import static org.junit.jupiter.api.Assertions.assertFalse;
 // import static org.junit.jupiter.api.Assertions.assertNotNull;
 // import static org.junit.jupiter.api.Assertions.assertNull;
 // import static org.junit.jupiter.api.Assertions.assertTrue;
-
-// import java.util.HashMap;
-// import java.util.List;
-// import java.util.Map;
-
+//
+//
 // import org.springframework.http.MediaType;
 // import org.junit.jupiter.api.Test;
-// import org.mockito.Mock;
 // import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.beans.factory.annotation.Value;
 // import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-// import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 // import org.springframework.boot.test.context.SpringBootTest;
-// import org.springframework.boot.test.mock.mockito.MockBean;
 // import org.springframework.test.web.servlet.MockMvc;
 // import org.springframework.test.web.servlet.MvcResult;
 // import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 // import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
+//
 // import com.fasterxml.jackson.databind.ObjectMapper;
 // import com.scalable.app.model.Cart;
 // import com.scalable.app.service.CartService;
-
+//
 // @SpringBootTest
 // @AutoConfigureMockMvc
 // class AppApplicationTests {
-    
+//    
 // 	@Autowired
 //     private MockMvc mockMvc;
-
+//
 // 	@Autowired
 // 	private ObjectMapper objectMapper;
-
+//
 // 	@Autowired
 // 	private CartService cartService;
-
+//
 // 	@Test
 // 	void contextLoads() {
 // 	}
-    
-
-
+//    
+//
+//
 // 	 @Test
 //     void testEditItemInCart_UpdatedCart() {
 //         Cart result = cartService.editItemInCart(1, "item1", "newItem1");
-
+//
 //         assertNotNull(result, "Cart should not be null");
 //         assertTrue(result.getItems().contains("newItem1"), "New item should be added");
 //         assertFalse(result.getItems().contains("item1"), "Old item should be removed");
 //     }
-
+//
 //     @Test
 //     void testEditItemInCart_CartNotFound() {
 //         Cart result = cartService.editItemInCart(4, "item1", "newItem");
-
+//
 //         assertNull(result, "Cart should be null if cartId doesn't exist");
 //     }
-
+//
 // 	@Test
 //     void testEditItemInCartEndpoint() throws Exception {
 //          MvcResult result = mockMvc.perform(MockMvcRequestBuilders
@@ -73,15 +66,15 @@
 //                     .contentType(MediaType.APPLICATION_JSON))
 //             .andExpect(MockMvcResultMatchers.status().isOk())  // Check if the status is OK (200)
 //             .andReturn();
-
+//
 // 		String responseContent = result.getResponse().getContentAsString();
-
+//
 // 		Cart responseCart = objectMapper.readValue(responseContent, Cart.class);
 // 		assertNotNull(responseCart);
 // 		assertEquals(2, responseCart.getId());
 // 		assertTrue(responseCart.getItems().contains("newItem3"));
 //     }
-
-	
-
+//
+//	
+//
 // }
